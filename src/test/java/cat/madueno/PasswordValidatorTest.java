4f9a90505d32a505aa8f38ed1f99936390c04930
Maribel Madueño <mmaduen4@xtec.cat>
2020-07-06 12:33:33 +0200

    versió 1: PasswordValidator + PasswordValidatorTest

---
+++ src/test/java/cat/madueno/PasswordValidatorTest.java
@@ -0,0 +1,43 @@
+package cat.madueno;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class PasswordValidatorTest {
+
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+
+	static Stream<Arguments> createContrasenyesValides() {
+		return Stream.of(Arguments.of("abcdefg1"), Arguments.of("ABCDEFG1"),Arguments.of("abcdef11"), Arguments.of("ABCDEF11"),
+				Arguments.of("aBCDEF11"));
+	}
+	
+	static Stream<Arguments> createContrasenyesNoValides() {
+		return Stream.of(Arguments.of("abcdef1"), Arguments.of("ABCDEF1"),Arguments.of("abcdefgh"), Arguments.of("ABCDEFGH"));
+	}
+
+	@ParameterizedTest
+	@MethodSource("createContrasenyesValides")	
+	public void testContrasenyaValida(String contrasenya)
+	{
+		PasswordValidator validador=new PasswordValidator(contrasenya);		
+		assertTrue(validador.validate());
+	}
+	
+	@ParameterizedTest
+	@MethodSource("createContrasenyesNoValides")	
+	public void testContrasenyaNoValida(String contrasenya)
+	{
+		PasswordValidator validador=new PasswordValidator(contrasenya);		
+		assertFalse(validador.validate());
+	}
+	
+	
+}
