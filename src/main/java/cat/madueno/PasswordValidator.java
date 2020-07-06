e505df9b16ced58d2c2bfda09c4af37c450808d4
Maribel Madueño <mmaduen4@xtec.cat>
2020-07-06 12:53:56 +0200

    Versió 2: PasswordValidator + PasswordValidatorTest

--- src/main/java/cat/madueno/PasswordValidator.java
+++ src/main/java/cat/madueno/PasswordValidator.java
@@ -2,7 +2,14 @@ package cat.madueno;
 
 public class PasswordValidator {
 	
-	private String pattern="^(?=.*[0-9])(?=.*[a-zA-Z]).{8,}$";
+	
+	/**
+	 *  Les contrasenyes han de tenir 10 caràcters com a mínim, almenys dos números 
+	 *  i hi ha d’haver lletres en majúscules i en minúscules.
+	 *  Falta la última condició de l'exercici 
+	 *  (almenys un símbol que no pot estar ni al principi ni al final). 
+	 */
+	private String pattern="^(?=(.*\\d){2})(?=.*[a-z])(?=.*[A-Z]).{10,}$";
 	private String password;
 	
 	public PasswordValidator(String password)
